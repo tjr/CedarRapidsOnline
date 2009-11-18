ac26da727d126397d839d75966d729631cc7df3e
Trevis Rothwell <tjr@acm.org>
2009-11-17 22:12:40 -0600

    added new event form

--- events.py
+++ events.py
@@ -63,9 +63,13 @@ class EventsPage:
                                                 "<div class=\"error\">Can't get event data.</div>\n")
             # Build the page.
             pagetext = "<ul>\n"
+            most_recent_month = None
             for result in results:
                 start_date = result[sqlutils.getfieldindex("start_date", description)]
                 end_date = result[sqlutils.getfieldindex("end_date", description)]
+                if (most_recent_month <> pageutils.get_month (start_date)):
+                    most_recent_month = start_date
+                    pagetext += "<h3>" + most_recent_month + "</h3>\n"
                 pagetext += ("<li>" + "<a href=\"/events/" +
                              result[sqlutils.getfieldindex("event_id", description)] + "\">" +
                              result[sqlutils.getfieldindex("title", description)] + "</a> (" +
@@ -73,6 +77,7 @@ class EventsPage:
                 if (result[sqlutils.getfieldindex("end_date")] <> None):
                     pagetext += " - " + pageutils.get_month (end_date) + " " + pageutils.get_day (end_date)
                 pagetext += ")</li>\n"
+            pagetext += "</ul>\n"
             return pageutils.generate_page ("Events", pagetext)
                     
         # Show specific event.
@@ -82,6 +87,44 @@ class EventsPage:
 
     def new (self):
         # Create new event, available to logged in users.
-        pass
+
+        # Verify user is logged in.
+        if (not pageutils.is_logged_in_p()):
+            raise cherrypy.HTTPRedirect ("/login")
+        
+        # Form to create new event.
+        pagecontents = ""
+        pagecontents += "<form action=\"/events/process\" method=\"post\">"
+        pagecontents += "<b>Title</b>:"
+        pagecontents += "<br>"
+        pagecontents += "<input type=\"text\" name=\"title\">"
+        pagecontents += "<br><br>\n"
+        pagecontents += "<b>Description</b>:"
+        pagecontents += "<br>"
+        pagecontents += "<textarea cols=80 rows=10 name=\"description\"></textarea>\n"
+        pagecontents += "<br><br>"
+        pagecontents += "<b>Start Date</b>:"
+        pagecontents += "<br>"
+        pagecontents += "<select name=\"start_month\">\n"
+        for month in [["01", "January"], ["02", "February"], ["03", "March"], ["04", "April"],
+                      ["05", "May"], ["06", "June"], ["07", "July"], ["08", "August"], ["09", "September"],
+                      ["10", "October"], ["11", "November"], ["12", "December"]]:
+            pagecontents += "<option value=\"" + month[0] + "\">" + month[1] + "</option>\n"
+        pagecontents += "</select>\n"
+        pagecontents += "<select name=\"start_month\">\n"
+        for day in ["01", "02", "03", "04", "05", "06", "07", "08", "09",
+                    "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                    "19", "20", "21", "22", "23", "24", "25", "26", "27",
+                    "28", "29", "30", "31"]:
+            pagecontents += "<option value=\"" + day + "\">" + day + "</option>\n"
+        pagecontents += "</select>\n"
+        pagecontents += "<select name=\"start_month\">\n"
+        for year in ["2009", "2010", "2011", "2012"]:
+            pagecontents += "<option value=\"" + year + "\">" + year + "</option>\n"
+        pagecontents += "</select>\n"
+        pagecontents += "<br><br>\n"
+        pagecontents += "<input type=\"submit\" value=\"Create New Event\">"
+        pagecontents += "</form>"
+        return pageutils.generate_page ("Create New Event", pagecontents)
     new.exposed = True
 
